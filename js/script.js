bcecb80db42c19fdf115b503e4385a8993035e85
PerezCaifas <64927681+PerezCaifas-dev@users.noreply.github.com>
2026-07-31 11:24:46 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("Invitación cargada");
+const seal = document.querySelector(".seal");
+const closed = document.querySelector(".closed");
+const open = document.querySelector(".open");
+
+seal.addEventListener("click", () => {
+
+    closed.style.display = "none";
+
+    seal.style.display = "none";
+
+    open.style.display = "block";
+
+});
