779c693299e6cdbfbbd76c157e6101af2bfc402c
PerezCaifas <64927681+PerezCaifas-dev@users.noreply.github.com>
2026-07-31 13:43:51 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@ const seal = document.querySelector(".seal");
 const closed = document.querySelector(".closed");
 const open = document.querySelector(".open");
 const music = document.getElementById("backgroundMusic");
+const letter = document.querySelector(".letter");
 
 seal.addEventListener("click", () => {
     music.play();
     closed.style.display = "none";
     seal.style.display = "none";
     open.style.display = "block";
+    letter.style.display = "block";
+    // letter.classList.add("extract");
 
     // setTimeout(() => {
     //     music.play().catch(error => {
@@ -15,4 +18,12 @@ seal.addEventListener("click", () => {
     //     });
     // }, 300);
 
+});
+
+open.addEventListener("click", () => {
+    letter.style.display = "block";
+    letter.style.zIndex = "10";
+    letter.classList.add("extract");
+    open.style.display = "none";
+    letter.classList.add("fullscreen");
 });
