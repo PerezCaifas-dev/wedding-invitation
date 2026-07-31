237b3c237ef9a4ef2b01bca23d6ff880bf48b1f1
PerezCaifas <64927681+PerezCaifas-dev@users.noreply.github.com>
2026-07-31 12:00:51 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 const seal = document.querySelector(".seal");
 const closed = document.querySelector(".closed");
 const open = document.querySelector(".open");
+const music = document.getElementById("backgroundMusic");
 
 seal.addEventListener("click", () => {
-
+    music.play();
     closed.style.display = "none";
-
     seal.style.display = "none";
-
     open.style.display = "block";
+
+    // setTimeout(() => {
+    //     music.play().catch(error => {
+    //         console.log("No se pudo reproducir el audio:", error);
+    //     });
+    // }, 300);
 
 });
